e6e3463269e791824d9c52ab90000d9669ef0fd9
Angelina <angelinarwhite@gmail.com>
2022-04-25 16:20:37 -0400

    commit tree d5cbb704bd742906c7054c6a8394a700aea818ac

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 36;
+let num4 = 8;
+let mod = num3%num4;
+
+let max = Math.max(0, 5, 10, 15, 20);
